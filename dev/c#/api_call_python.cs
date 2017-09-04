763ab2f3793ef70ed515d97753a61eaa3bdea183
AlexTian <atflysun@outlook.com>
2017-09-04 10:17:16 +0800

    add c# call python api

---
+++ dev/c#/api_call_python.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://120.55.168.18:8839/job_parse";
+            string Text = File.ReadAllText(@"D:\a.json");
+
+            //var parameterJson = Newtonsoft.Json.JsonConvert.SerializeObject(new {  Text });
+            HttpClient hcParse = new HttpClient();
+
+            var responseParse = hcParse.PostAsync(url, new StringContent(Text, System.Text.Encoding.UTF8, "application/text")).Result;
+            var contentParse = responseParse.Content.ReadAsStringAsync().Result; 
+            var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(contentParse);
+            //Console.WriteLine(obj.ExtractResult.Value.Text);
+        }
+    }
+}
